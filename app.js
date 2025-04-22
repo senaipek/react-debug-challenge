ebacc60bdfaf01623a1d1929861a0c4821a8371a
senaipek <seenaipek@gmail.com>
2025-04-22 14:27:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 document.getElementById("showBtn").addEventListener("click", function() {
-  const message = document.querySelector("#mesage"); // Hatalı kısım
+  const message = document.querySelector("#mesage"); // 
   message.style.display = "block";
 });
