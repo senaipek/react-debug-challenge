67306898753565066359996dfb27b5b5a60e0a10
senaipek <seenaipek@gmail.com>
2025-04-22 13:54:14 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("showBtn").addEventListener("click", function() {
+  const message = document.querySelector("#mesage"); // Hatalı kısım
+  message.style.display = "block";
+});
